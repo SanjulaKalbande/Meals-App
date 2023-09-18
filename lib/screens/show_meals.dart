2d2c612f1d18c1951e5b43e287e67a313a186b4d
Sanjula Kalbande <sanjullaa12@gmail.com>
2023-09-18 19:42:16 +0530

    second commit with meals op

---
+++ lib/screens/show_meals.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+//go to this screen if one of the items is tapped
+//to show all the meals
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({Key? key}) : super(key: key);
+
+final String title;
+final meal
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text,
+      ),
+    );
+  }
+}
